2b116e025a1f5f8f3b9a333ae45104ed7c3ea3e2
xiaoluo <xiaoluo@luo.com>
2018-03-05 21:49:50 +0800

    clean code

--- leetcode/4_median_of_two_sorted_arrays.py
+++ leetcode/4_median_of_two_sorted_arrays.py
@@ -9,72 +9,6 @@ def get_two_median_num_index_from_list(numslist):
     return indexs_list
 
 
-def get_max_val_from_two_list(left_mini_nums, left_max_nums):
-    if len(left_mini_nums) == 0:
-        return left_max_nums[-1], left_max_nums
-    if len(left_max_nums) == 0:
-        return left_mini_nums[-1], left_mini_nums
-    return left_mini_nums[-1], left_mini_nums if left_mini_nums[-1] > left_max_nums[-1] else left_max_nums[-1], left_max_nums
-
-
-def get_mini_val_from_two_list(right_mini_nums, right_max_nums):
-    if len(right_mini_nums) == 0:
-        return right_max_nums[0], right_max_nums
-    if len(right_mini_nums) == 0:
-        return left_mini_nums[0], left_mini_nums
-    return right_mini_nums[0], right_mini_nums if right_mini_nums[0] < right_max_nums[0] else right_max_nums[0], right_max_nums
-
-
-class MedianItem(object):
-    def __init__(self, num_index, num_list):
-        self.num_index = num_index
-        self.num_list = num_list
-
-    def get_val(self):
-        return self.num_list[self.num_index]
-
-
-class MeidanStack(object):
-    def __init__(self):
-        self.items = []
-
-    def add(self, medianitem):
-        medianitem_val = medianitem.get_val()
-        if len(self.items) == 0:
-            self.items.append(medianitem)
-            return None, None
-        if len(self.items) == 1:
-            if medianitem_val >= self.items[0]:
-                self.items.append(medianitem)
-            else:
-                self.items.insert(0, medianitem)
-            return None, None
-        else:
-            if medianitem_val <= self.items[0]:
-                self.items.insert(0, medianitem)
-                right_popup = self.items[-1]
-                self.items = self.items[:2]
-                return None, right_popup
-            elif medianitem_val >= self.items[1]:
-                self.items.append(medianitem)
-                left_popup = self.items[0]
-                self.items = self.items[-2:]
-                return left_popup, None
-            else:
-                left_popup, right_popup = self.items[0], self.items[1]
-                self.items = [medianitem]
-                return left_popup, right_popup
-
-    def __str__(self):
-        if len(self.items) == 2:
-            s = '%s , %s' % (self.items[0], self.items[1])
-        elif len(self.items) == 1:
-            s = '%s' % self.items[0]
-        else:
-            s = ''
-        return s
-
-
 def gen_new_median_list(median_list, num):
     if len(median_list) == 0:
         median_list.append(num)
@@ -227,10 +161,6 @@ if __name__ == '__main__':
     # numslist = [1,2,3,4,6]
     # print(get_two_median_num_index_from_list(numslist))
 
-    # ms = MeidanStack()
-    # for item in [2, 10,9,4,6,1]:
-    #   ms.add(item)
-    #   print(ms)
 
     numslist1 = [1,4,7,9,18,20, 39, 500]
     numslist2 = [5,11,17,25,190,210]
